ae5c76adcc8e5dcd967508d56c830233644e102f
Sovimi <sofia_vieira@live.com.pt>
2018-07-13 16:55:17 +0100

    Added the backend for the user register and login

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
 
 var wine = require('./routes/wine');
-var app = express();
+var auth = require('./routes/index');
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/wineCellar', { useMongoClient: true, promiseLibrary: require('bluebird') })
+mongoose.connect('mongodb://localhost/wineCellar', {promiseLibrary: require('bluebird') })
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
+
+require('./config/passport');
+
+var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -19,6 +25,15 @@ app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/wines', express.static(path.join(__dirname, 'dist')));
 app.use('/wine', wine);
+app.use(cookieParser());
+//app.use(cors());
+
+// [SH] Initialise Passport before using the route middleware
+app.use(passport.initialize());
+
+// [SH] Use the API routes when path starts with /api
+app.use('/', auth);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/User.js
+++ models/User.js
@@ -16,17 +16,17 @@ var UserSchema = new mongoose.Schema({
     salt: String
   });
 
-  userSchema.methods.setPassword = function(password){
+  UserSchema.methods.setPassword = function(password){
     this.salt = crypto.randomBytes(16).toString('hex');
     this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
   };
   
-  userSchema.methods.validPassword = function(password) {
+  UserSchema.methods.validPassword = function(password) {
     var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
     return this.hash === hash;
   };
   
-  userSchema.methods.generateJwt = function() {
+  UserSchema.methods.generateJwt = function() {
     var expiry = new Date();
     expiry.setDate(expiry.getDate() + 7);
   
